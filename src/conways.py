8bcf8e36fa7cecd7b788c6934bab45b9ee40746d
Talent Antonio <antonio.talenty@gmail.com>
2019-07-17 16:37:57 +0100

    for loop to get random

--- src/conways.py
+++ src/conways.py
@@ -4,13 +4,32 @@ import pygame, random
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 GRAY = (25, 25, 25)
-WIN_SIZE = 500
+RED = (255, 0, 0)
+MARGIN = 3
+SQ_LENGTH = 20
+SQ_NUM = 25
+WIN_SIZE = (SQ_NUM + 1) * MARGIN + SQ_NUM * SQ_LENGTH
 
 pygame.init()
  
 # Set the width and height of the screen [width, height]
 size = (WIN_SIZE, WIN_SIZE)
 screen = pygame.display.set_mode(size)
+
+automata = [0] * (SQ_NUM * SQ_NUM)
+
+# Assign Random Values to our Automata
+# rowx:
+#     col:
+#         automata[row * SQ_NUM + col] = set to a random number
+
+# for i in range(SQ_NUM * SQ_NUM):
+#     automata[i] = random.randint(0, 1)
+
+for row in range(SQ_NUM):
+    for col in range(SQ_NUM):
+        automata[row * SQ_NUM + col] = random.randint(0, 1)
+
 
 # Add a title
 pygame.display.set_caption("Conway's Game of Life")
@@ -37,9 +56,21 @@ while not done:
     # Here, we clear the screen to gray. Don't put other drawing commands
     # above this, or they will be erased with this command.
     screen.fill(GRAY)
- 
+    automata[12] = 1
     # --- Drawing code should go here
-   
+    # pygame.draw.rect(screen, RED, pygame.Rect(20, 20, 20, 20))
+    y = MARGIN
+    i = 0
+    while y < WIN_SIZE:
+        x = MARGIN
+        while x < WIN_SIZE:
+            if automata[i] == 0:
+                pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, SQ_LENGTH, SQ_LENGTH))
+            else:
+                pygame.draw.rect(screen, WHITE, pygame.Rect(x, y, SQ_LENGTH, SQ_LENGTH))
+            i += 1
+            x += SQ_LENGTH + MARGIN
+        y += SQ_LENGTH + MARGIN
 
     # --- Go ahead and update the screen with what we've drawn.
     pygame.display.flip()
